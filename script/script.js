4714d471afbf1988b7aa392d03d633d645563f00
João Victor Gomes <95916167+jv-gb@users.noreply.github.com>
2022-06-07 12:41:03 -0300

    Add files via upload
    Botão que utiliza o DOM para modificar a posição de elementos html de maneira divertida

---
+++ script/script.js
@@ -0,0 +1,50 @@
+function yesStupid() {
+  //Exibe a mensagem "eu sabia"
+  let iKnew = document.getElementById("iKnew")
+  iKnew.style.display = "block"
+
+  //Esconde a pergunta "você é estúpido?"
+  let areYou = document.getElementById("areYou")
+  areYou.style.display = "none"
+}
+
+
+function noStupid() {
+  let nao = document.getElementById("nao")
+  let nao1 = document.getElementById("nao1")
+
+  nao.style.display = "none "
+  nao1.style.display = "inline-block"
+  nao1.style.marginLeft = "100px"
+}
+
+function noStupid1() {
+  let nao1 = document.getElementById("nao1")
+  let nao2 = document.getElementById("nao2")
+
+  nao1.style.display = "none "
+  nao2.style.display = "inline-block"
+  nao2.style.marginTop = "100px"
+}
+
+function noStupid2() {
+  let nao2 = document.getElementById("nao2")
+  let nao3 = document.getElementById("nao3")
+
+  nao2.style.display = "none "
+  nao3.style.display = "inline-block"
+  nao3.style.marginLeft = "300px"
+}
+
+
+function noStupid3() {
+  let nao = document.getElementById("nao")
+  let nao3 = document.getElementById("nao3")
+
+  nao3.style.display = "none "
+  nao.style.display = "inline-block"
+
+}
+
+
+
